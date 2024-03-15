fa3cc4af09d9892909ffcb930de1ac45f7f37d25
aboyreau <aboyreau@student.42lehavre.fr>
2024-03-15 05:48:52 +0100

    printf norminette

--- libft/printf/ft_printf.c
+++ libft/printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: aboyreau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 20:20:37 by aboyreau          #+#    #+#             */
-/*   Updated: 2024/03/14 08:14:48 by aboyreau         ###   ########.fr       */
+/*   Updated: 2024/03/15 05:47:42 by aboyreau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,32 +36,6 @@ char	*format_str(const char **str, va_list lst)
 	return (chain);
 }
 
-int	ft_vsprintf(char *buf, const char *str, va_list lst)
-{
-	int		cpt;
-	char	*chain;
-
-	cpt = 0;
-	buf[0] = '\0';
-	while (*str)
-	{
-		if (*str != '%')
-		{
-			buf[cpt++] = *str++;
-			buf[cpt] = '\0';
-		}
-		else if (*str == '%')
-		{
-			chain = format_str(&str, lst);
-			cpt += ft_strlen(chain);
-			ft_strlcat(buf, chain, cpt + ft_strlen(chain) + 1);
-			free(chain);
-		}
-		str++;
-	}
-	return (cpt);
-}
-
 int	ft_sprintf(char *buf, const char *str, ...)
 {
 	va_list		lst;
@@ -71,27 +45,6 @@ int	ft_sprintf(char *buf, const char *str, ...)
 	va_start(lst, str);
 	cpt = ft_vsprintf(buf, str, lst);
 	va_end(lst);
-	return (cpt);
-}
-
-int	ft_vdprintf(int fd, const char *str, va_list lst)
-{
-	int		cpt;
-	char	*chain;
-
-	cpt = 0;
-	while (*str)
-	{
-		if (*str != '%')
-			cpt += write(1, str, 1);
-		else if (*str == '%')
-		{
-			chain = format_str(&str, lst);
-			cpt += write(fd, chain, ft_strlen(chain));
-			free(chain);
-		}
-		str++;
-	}
 	return (cpt);
 }
 
