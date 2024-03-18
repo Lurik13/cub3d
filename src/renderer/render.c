4473267f6645613f84e58844c27efad4fef84adb
aboyreau <aboyreau@student.42lehavre.fr>
2024-03-18 07:45:56 +0100

    petites fonctions à la norme

--- src/renderer/render.c
+++ src/renderer/render.c
@@ -6,18 +6,14 @@
 /*   By: lribette <lribette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 06:37:11 by aboyreau          #+#    #+#             */
-/*   Updated: 2024/03/18 06:53:24 by aboyreau         ###   ########.fr       */
+/*   Updated: 2024/03/18 07:45:12 by aboyreau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "render.h"
-#include "../libft/math/math.h"
-#include "cub3d.h"
 #include "vector/vector.h"
-#include <float.h>
-#include <math.h>
 
-static const int colors[256] = {\
+static const int	g_colors[256] = {\
 	['1'] = 0b00000000011110000001111000000000, \
 	['0'] = 0b00000000000000000111100001111000, \
 	['.'] = 0b00000000000111100110010000000000
@@ -30,7 +26,7 @@ void	get_ray_direction(double ray[2], t_2dvector camera, t_2dvector fov,
 	ray[DIRY] = camera.h + fov.h * cameraX;
 }
 
-void	get_ray_dist_per_step(double ray_dist[2], double	ray[2])
+void	get_ray_dist_per_step(double ray_dist[2], double ray[2])
 {
 	if (ray[DIRX] != 0)
 		ray_dist[DIRX] = ft_abs(1 / ray[DIRX]);
@@ -53,33 +49,29 @@ double	get_distance_per_column(int column, t_game *game)
 	double		ray_pos[2];
 	double		ray_dist_per_step[2];
 	t_2dvector	fov;
-	double		cameraX;
+	double		camerax;
 
 	fov.h = 0;
 	fov.v = 0.66f;
-	cameraX = 2 * column / (double)WIDTH - 1;
+	camerax = 2 * column / (double)WIDTH - 1;
 	ray_pos[DIRX] = game->player->position->h;
 	ray_pos[DIRY] = game->player->position->v;
-	get_ray_direction(ray, *game->player->camera, fov, cameraX);
+	get_ray_direction(ray, *game->player->camera, fov, camerax);
 	get_ray_dist_per_step(ray_dist_per_step, ray);
-	while (ray_pos[DIRX] < ft_tablen(game->map) &&
-			ray_pos[DIRY] < game->longest_line && 
+	while (ray_pos[DIRX] < ft_tablen(game->map) && \
+			ray_pos[DIRY] < game->longest_line && \
 			game->map[(int)ray_pos[DIRX]][(int)ray_pos[DIRY]] != '1')
 	{
 		ray_pos[DIRX] += ray_dist_per_step[DIRX];
 		ray_pos[DIRY] += ray_dist_per_step[DIRY];
 	}
-	if (ray_pos[DIRX] >= ft_tablen(game->map) ||
+	if (ray_pos[DIRX] >= ft_tablen(game->map) || \
 			ray_pos[DIRY] >= game->longest_line)
 		return (0);
 	return (pythagoras(ray_pos[DIRX] - game->player->position->h,
 			ray_pos[DIRY] - game->player->position->v));
 }
 
-# define SCALE_FACTOR 24
-# define PLAYER_SIZE 6
-
-//////////////////////////////////////////////////////////////////////////////// mise a jour de display_map
 void	display_square(t_game *game, int colour, int i, int j)
 {
 	int	k;
@@ -91,14 +83,90 @@ void	display_square(t_game *game, int colour, int i, int j)
 		l = 0;
 		while (l < SCALE_FACTOR)
 		{
-			mlx_pixel_put(game->texture->mlx, game->texture->window, i * SCALE_FACTOR + k, j * SCALE_FACTOR + l, colour);
+			mlx_pixel_put \
+			(
+				game->texture->mlx, \
+				game->texture->window, \
+				i * SCALE_FACTOR + k, \
+				j * SCALE_FACTOR + l, \
+				colour \
+			);
 			l++;
 		}
 		k++;
 	}
 }
 
-void	display_map(void *mlx, void *window, t_game *game)
+void	display_player(t_game *game)
+{
+	int	k;
+	int	l;
+
+	k = 0;
+	while (k < PLAYER_SIZE)
+	{
+		l = 0;
+		while (l < PLAYER_SIZE)
+		{
+			mlx_pixel_put(
+				game->texture->mlx, \
+				game->texture->window, \
+				game->player->position->v * SCALE_FACTOR + k, \
+				game->player->position->h * SCALE_FACTOR + l, \
+				ft_color(230, 200, 0) \
+			);
+			l++;
+		}
+		k++;
+	}
+}
+
+/**
+ * @brief Propagate a ray until a wall is met.
+ * @param `game` The game structure.
+ * @param `start_position` The column of the screen from which the ray
+ * should start.
+ */
+void	send_ray(t_game *game, int start_position)
+{
+	double		ray[2];
+	double		*ray_pos;
+	double		distance;
+	double		ray_dist_per_step[2];
+	t_2dvector	fov;
+
+	ray_pos = (double []){game->player->position->v, game->player->position->h};
+	fov = (t_2dvector){.h = 0.66f, .v = 0};
+	get_ray_direction(ray, *game->player->camera, fov, start_position);
+	ray_dist_per_step[DIRX] = 1;
+	ray_dist_per_step[DIRY] = 0;
+	while (game->map[(int)ray_pos[DIRY]][(int)ray_pos[DIRX]] != '1')
+	{
+		ray_pos[DIRX] += ray_dist_per_step[DIRX];
+		ray_pos[DIRY] += ray_dist_per_step[DIRY];
+	}
+	if ((int) game->player->position->h == (int) ray_pos[DIRY])
+		ray_pos[DIRX] = (int)ray_pos[DIRX];
+	else
+		ray_pos[DIRY] = (int)ray_pos[DIRY];
+	distance = pythagoras(
+			ray_pos[DIRX] - game->player->position->v,
+			ray_pos[DIRY] - game->player->position->h \
+		);
+}
+// printf("Player is at %'.2lf %'.2lf, the wall is at %'.2lf %'.2lf\n", 
+// 	game->player->position->v, game->player->position->h,
+// 	ray_pos[DIRX], ray_pos[DIRY]
+// );
+// printf("The ray travelled %'.2lf\n", distance);
+
+void	display_player_orientation(t_game *game)
+{
+	send_ray(game, WIDTH / 2);
+}
+
+// Le if sur le caractere n permet d'eviter l'effet limace
+void	display_map(t_game *game)
 {
 	int	i;
 	int	j;
@@ -110,67 +178,40 @@ void	display_map(void *mlx, void *window, t_game *game)
 		while (j < ft_tablen(game->map))
 		{
 			if (game->map[j][i] == 'n')
-				display_square(game, 0, i, j); // fonction tue-limace
-			display_square(game, colors[(int)game->map[j][i]], i, j);
+				display_square(game, 0, i, j);
+			display_square(game, g_colors[(int)game->map[j][i]], i, j);
 			j++;
+			display_player(game);
 		}
 		i++;
 	}
-	double	ray[2];
-	double	ray_pos[2] = {game->player->position->v, game->player->position->h};
-	double	ray_dist_per_step[2];
-	t_2dvector	fov = {.h = 0.66f, .v=0};
-
-	get_ray_direction(ray, *game->player->camera, fov, 1);
-	ray_dist_per_step[DIRX] = 1;
-	ray_dist_per_step[DIRY] = 0;
-	// printf("coucou\n");
-	while (
-		   
-			game->map[(int)ray_pos[DIRY]][(int)ray_pos[DIRX]] != '1')
-	{
-		ray_pos[DIRX] += ray_dist_per_step[DIRX];
-		ray_pos[DIRY] += ray_dist_per_step[DIRY];
-		// mlx_pixel_put(mlx, window, game->player->position->v * SCALE_FACTOR + i, game->player->position->h * SCALE_FACTOR + j, ft_color(255, 0, 255));
-		// mlx_pixel_put(mlx, window, ray_pos[DIRX], ray_pos[DIRY], ft_color(255, 0, 1));
-	}
-	// printf("Player is at %lf %lf, the wall is at %lf %lf\n", game->player->position->v, game->player->position->h, ray_pos[DIRX], ray_pos[DIRY]);
-	// le rayon va tout droit vers la droite.
-	for (int i = game->player->position->h; i < ray_pos[DIRX]; i++)
-	{
-		for (int j = game->player->position->v; j < ray_pos[DIRY]; j++)
-		{
-			mlx_pixel_put(mlx, window, i, j, ft_color(255, 0, 255));
-		}
-	}
+	display_player_orientation(game);
 }
 
 void	render(void *param)
 {
-	void *mlx;
-	void *window;
-	t_game *game;
-	double	ray_norm;
+	void	*mlx;
+	void	*window;
+	t_game	*game;
 
 	mlx = ((void **) param)[0];
 	window = ((void **) param)[1];
 	game = ((void **) param)[2];
 	game->player->camera->v = -1;
 	game->player->camera->h = 0;
-	display_map(mlx, window, game);
-	for (int x = 0; x < WIDTH; x++)
-	{
-		ray_norm = get_distance_per_column(x, game);
-		double	texture_height;
-
-		if (ray_norm)
-		{
-			texture_height = (double)HEIGHT * (1 / ray_norm);
-			for (int i = HEIGHT / 2 - texture_height / 2; i < HEIGHT / 2 + texture_height / 2; i++)
-				mlx_pixel_put(mlx, window, x, i, ft_color(255, 0, 0));
-		}
-	}
-	for (int k = 0; k < PLAYER_SIZE; k++)
-		for (int l = 0; l < PLAYER_SIZE; l++)
-			mlx_pixel_put(mlx, window, game->player->position->v * SCALE_FACTOR + k, game->player->position->h * SCALE_FACTOR + l, ft_color(230, 200, 0));
+	display_map(game);
 }
+// raycasting but it doesnt work
+// double	ray_norm;
+// for (int x = 0; x < WIDTH; x++)
+// {
+// 	ray_norm = get_distance_per_column(x, game);
+// 	double	texture_height;
+// 	if (ray_norm)
+// 	{
+// 		texture_height = (double)HEIGHT * (1 / ray_norm);
+// 		for (int i = HEIGHT / 2 - texture_height / 2;
+// 		i < HEIGHT / 2 + texture_height / 2; i++)
+// 			mlx_pixel_put(mlx, window, x, i, ft_color(255, 0, 0));
+// 	}
+// }
