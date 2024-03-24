d35e13ec914c30a781ff91d807dcd53810620c15
aboyreau <aboyreau@student.42lehavre.fr>
2024-03-24 13:49:26 +0100

    parser now handles colors error

--- src/parser/parse_attrs.c
+++ src/parser/parse_attrs.c
@@ -6,7 +6,7 @@
 /*   By: aboyreau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 10:24:59 by aboyreau          #+#    #+#             */
-/*   Updated: 2024/03/15 11:45:27 by aboyreau         ###   ########.fr       */
+/*   Updated: 2024/03/24 13:17:48 by aboyreau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,49 +17,57 @@
 #include "str/ft_str.h"
 #include <unistd.h>
 
+int	ft_strcount(char c, char *str)
+{
+	int	count;
+
+	count = 0;
+	while (*str)
+	{
+			if (*str == c)
+				count++;
+			str++;
+	}
+	return (count);
+}
+
+/**
+ * @return 0 if no colors were found, 1 if a valid floor or ceiling color
+ * was found, -1 if an invalid floor or ceiling is found.
+ */
 int	parse_colors(char *line, t_game **game)
 {
-	if (ft_strstartswith(line, "C"))
+	int	r;
+	int	g;
+	int	b;
+
+	if (ft_strstartswith(line, "C") || ft_strstartswith(line, "F"))
 	{
-		(*game)->texture->ceiling = ft_color(\
-			ft_atoi(line + 2), \
-			ft_atoi(ft_strchr(line, ',')), \
-			ft_atoi(ft_strrchr(line, ',')) \
-		);
-		return (1);
-	}
-	if (ft_strstartswith(line, "F"))
-	{
-		(*game)->texture->floor = ft_color(\
-			ft_atoi(line + 2), \
-			ft_atoi(ft_strchr(line, ',')), \
-			ft_atoi(ft_strrchr(line, ',')) \
-		);
+		if (ft_strcount(',', line) != 2)
+			return (-1);
+		r = ft_atoi(line + 2);
+		g = ft_atoi(ft_strchr(line, ',') + 1);
+		b = ft_atoi(ft_strrchr(line, ',') + 1);
+		if (r < 0 || r > 255 || g < 0 || g > 255 | b < 0 || b > 255)
+			return (-1);
+		if (ft_strstartswith(line, "C"))
+			(*game)->texture->ceiling = ft_color(r, g, b);
+		else
+			(*game)->texture->floor = ft_color(r, g, b);
 		return (1);
 	}
 	return (0);
 }
 
-/*
-void	*load_texture(char *path)
-{
-	int		index;
-	void	*texture;
-
-	texture = mlx_xpm_file_to_image(mlx, path, &index, &index);
-	if (texture == NULL)
-		ft_dprintf(STDERR_FILENO, "Cannot load the texture %s\n", path);
-	return (texture);
-}
-*/
-
 int	parse_attr(char *line, t_game **game)
 {
 	int	direction;
+	int	err;
 
 	direction = -1;
-	if (parse_colors(line, game))
-		return (0);
+	err = parse_colors(line, game);
+	if (err)
+		return (err - 1);
 	if (ft_strstartswith(line, "NO"))
 		direction = NORTH;
 	else if (ft_strstartswith(line, "SO"))
